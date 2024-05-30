497f896ebcf66f9ac80ed68160432228b9e44d56
Godswill Azubike <info.godswillazubike@gmail.com>
2024-05-30 10:25:21 +0100

    student calender feature

--- resources/views/livewire/admin/dashboard/event.blade.php
+++ resources/views/livewire/admin/dashboard/event.blade.php
@@ -26,7 +26,7 @@
 
 
 
-        <div class="modal fade" id="eventModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        {{-- <div class="modal fade" id="eventModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
             aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
@@ -60,7 +60,41 @@
                     </form>
                 </div>
             </div>
+        </div> --}}
+            <div class="modal fade" id="eventModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <form method="post" action="{{route('admin.calenderAjax')}}" class="form-group">
+                @csrf
+                <div class="modal-header">
+                <h5 class="modal-title">Add Event</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <div class="form-group mb-2">
+                    <label>Title</label>
+                    <input class="form-control" name="title" type="text" required>
+                </div>
+                <input type="hidden" name="type" value="add">
+                <div class="form-group mb-2">
+                    <label>Start</label>
+                    <input class="form-control" name="start" type="datetime-local" required>
+                </div>
+                <div class="form-group mb-2">
+                    <label>End</label>
+                    <input class="form-control" name="end" type="datetime-local" required>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="submit" class="btn btn-primary">Save Event</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              </div>
+            </form>
         </div>
+      </div>
+    </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
